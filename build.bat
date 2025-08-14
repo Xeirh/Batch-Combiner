34d82d2e054a56c5b8b11a52b0803d11e1397352
Ryan <108033967+Xeirh@users.noreply.github.com>
2025-08-14 16:26:02 -0500

    build.bat

--- build.bat
+++ build.bat
@@ -1,12 +1,34 @@
 @echo off
+
 setlocal enabledelayedexpansion
 
-:: mapping
-set MOD_ROOT=%CD%
-set MOD_PATH=%LOCALAPPDATA%\Plutonium\storage\t6\mods
-set "MOD_INPT=%MOD_ROOT%\initiative"
-set "MOD_OUPT=%MOD_ROOT%\scripts"
-set "MOD_MERG=%MOD_OUPT%\%MOD_NAME%.gsc"
+:: Mapping
+set MOD_PATH=%CD%
+
+for %%a in (%MOD_PATH%) do set MOD_NAME=%%~nxa
+
+set MOD_INPUT=%MOD_PATH%\<custom folder>
+set MOD_OUTPUT=%MOD_PATH%\<output folder>
+
+set MOD_OUTPUT_FINAL=%MOD_OUTPUT%\%MOD_NAME%.<file extension>
+
+:: Index input entries
+set INDEX=0
+
+for /r %MOD_INPUT% %%f in (*.gsc) do (
+    set /a INDEX+=1
+    set MEMORY[!INDEX!]=%%f
+)
+
+:: Merge all index input entries
+for /l %%n in (1,1,%INDEX%) do (
+    type !MEMORY[%%n]!>>%MOD_OUTPUT_FINAL%
+    if not %%n==%INDEX% (
+        (echo.)>>%MOD_OUTPUT_FINAL%
+        (echo.)>>%MOD_OUTPUT_FINAL%
+    )
+)
 
 pause
+
 endlocal
